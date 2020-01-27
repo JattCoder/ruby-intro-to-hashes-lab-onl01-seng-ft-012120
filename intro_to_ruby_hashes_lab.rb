65fd2c1a06ffa9451381feec592369897c85fb7e
Harmandeep Mand <harmandeepmand.hm@gmail.com>
2020-01-27 23:27:25 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -35,8 +35,7 @@ def monopoly_with_third_tier
 		:rent_in_dollars => {
 			:one_piece_owned => 25,
 			:two_piece_owned => 50,
-			:three_piece_owned => 100,
-			:four_piece_owned => 200
+			:three_piece_owned => 100
 		}
 	}
 	return monopoly
